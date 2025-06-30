51bed3fe86d690cebbd738a3c5f40afdc0c3a20f
RupakBoral <boralrupak@gmail.com>
2025-06-30 18:41:52 +0530

    first commit

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -3,30 +3,19 @@ import image from "../images/aboutimg.jpg";
 
 const AboutUs = () => {
   return (
-    <>
+    <div>
       <section className="container">
         <h2 className="page-heading about-heading">About Us</h2>
         <div className="about">
           <div className="hero-img">
-            <img
-              src={image}
-              alt="hero"
-            />
+            <img src={image} alt="hero" />
           </div>
           <div className="hero-content">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Quibusdam tenetur doloremque molestias repellat minus asperiores
-              in aperiam dolor, quaerat praesentium. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Voluptatibus, repudiandae! Lorem
-              ipsum dolor sit amet consectetur adipisicing elit. Provident
-              quibusdam doloremque ex? Officia atque ab dolore? Tempore totam
-              non ea!
-            </p>
+            <p>Project by Chandra Shekar and Rupak Boral.</p>
           </div>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
